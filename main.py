fc04e0eae11052581750fa09de88afd66650dbd8
AJaxx86 <62261949+AJaxx86@users.noreply.github.com>
2025-07-08 04:02:57 +0100

    stuff

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    gametime = pygame.time.Clock()
+    dt = 0
+
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
         
@@ -13,6 +16,8 @@ def main():
                 return
         
         pygame.display.flip()
+        dt = gametime.tick(60)
+
 
 if __name__ == "__main__":
     main()
